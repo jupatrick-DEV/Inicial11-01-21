5d4ad0eaa4cfeb5a73fae3524f8c0bbc6054a5ba
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-R4HJL4F>
2021-01-12 10:40:27 -0200

    Criação da Página inicial aula 12/01/21

--- index.php
+++ index.php
@@ -13,83 +13,78 @@
         <title>Portifólio</title>
     </head>
     <body>
-        <div class="container">
-
-            <div class="row">
-                <div class="col">
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Dropdown
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#">Ação</a>
-                                        <a class="dropdown-item" href="#">Outra ação</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Algo mais aqui</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled" href="#">Desativado</a>
-                                </li>
-                            </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-                            </form>
-                        </div>
-                    </nav>
-                    <button type="button" class="btn btn-outline-primary">O Programa</button>
-                    <button type="button" class="btn btn-outline-secondary">Grade Curricular</button>
-                    <button type="button" class="btn btn-outline-success">Corpo Docente</button>
-                    <button type="button" class="btn btn-outline-danger">Corpo Discente</button>
-                    <button type="button" class="btn btn-outline-warning">Egressos</button>
-                    <button type="button" class="btn btn-outline-info">Procedimentos Acadêmicos</button>
-                    <button type="button" class="btn btn-outline-dark">Contato</button>
-
-                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100" src="assets/img/Seleção.jpg" alt="Primeiro Slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Segundo Slide" alt="Segundo Slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Terceiro Slide" alt="Terceiro Slide">
-                            </div>
-                        </div>
+        <header class="container col-sm">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-item">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="assets/img/Seleção.jpg" alt="Primeiro Slide">
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Anterior</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Próximo</span>
-                    </a>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/bolsista.jpg" alt="Segundo Slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/disciplinas.jpg" alt="Terceiro Slide">
+                    </div>
                 </div>
-
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Próximo</span>
+                </a>
             </div>
+            
+            <!--cabecalho-->
+            <div><img src="" alt="" /></div>
+            <!--navegacap-->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <!--                <a class="navbar-brand" href="#">Navbar</a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>-->
+
+                <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="plugins/P1/dist/index.html">O Programa <span class="sr-only">(página atual)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="plugins/P1/dist/index.html">Grade Curricular <span class="sr-only">(página atual)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="plugins/P1/dist/index.html">Corpo Docente<span class="sr-only">(página atual)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="plugins/P4/dist/index.html">Corpo Discente <span class="sr-only">(página atual)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="plugins/P5/dist/index.html">Contato<span class="sr-only">(página atual)</span></a>
+                        </li>
+
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                    </form>
+                </div>
+            </nav>
+            <!--fim navegacao-->
+            
+                   
+
+                </div>
+            </div>
+
         </div>
-    </div>
-    <!-- JavaScript (Opcional) -->
-    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
+
+
+
+        <!-- JavaScript (Opcional) -->
+        <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
 </html>
